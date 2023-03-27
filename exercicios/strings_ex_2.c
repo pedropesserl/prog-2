2248a412c4703010fcb9823f22bd7c6c5d4d00fc
Pedro F. Pesserl <fpesserl7@gmail.com>
2023-03-27 18:28:27 -0300

    ex 2

--- exercicios/strings_ex_2.c
+++ exercicios/strings_ex_2.c
@@ -1,5 +1,23 @@
+/* Calcular o tamanho de uma string (sem usar strlen). */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: ./strings_ex_2 <string>\n");
+        exit(1);
+    }
+    
+    const char *input = argv[1];
 
+    int i = 0;
+    while (strcmp(&input[i], "\0") != 0) {
+        i++;
+    }
+
+    printf("Tamanho do input: %d\n", i);
+
+    return 0;
 }
