449928f776c68857b127ee0b7df42acaad397ab2
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-05-17 13:08:24 -0300

    parsing options

--- vinapp/vinapp.c
+++ vinapp/vinapp.c
@@ -2,12 +2,40 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-#define USAGE() do { \
-    printf("Uso:\n"); \
+#define USAGE_EXIT(err) do {   \
+    fprintf(stderr, "Uso:\n"); \
+    exit(1);                   \
 } while (0)
 
 int main(int argc, char **argv) {
     
+    char c = argv[1][1];
+    putchar(c);
+    putchar('\n');
+
+    char *nome;
+    nome = argv[2];
+    printf("%s\n", nome);
+
+    /* char c = getopt(argc, argv, "iamxrch"); */
+    /* switch (c) { */
+    /*     case 'i': */
+    /*         break; */
+    /*     case 'a': */
+    /*         break; */
+    /*     case 'm': */
+    /*         break; */
+    /*     case 'x': */
+    /*         break; */
+    /*     case 'r': */
+    /*         break; */
+    /*     case 'c': */
+    /*         break; */
+    /*     case 'h': */
+    /*         break; */
+    /*     default: */
+    /*         USAGE_EXIT(1); */
+    /* } */
 
     return 0;
 }
